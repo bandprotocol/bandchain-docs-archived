89a14001f1f43e5674715eb231b866c30da93151
Paul Nattapatsiri <pchonpimai@gmail.com>
2020-09-22 18:05:24 +0700

    Readding theme

---
+++ theme/vuepress-theme-cosmos/global-components/TmIconExpanded.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    width="16px"
+    height="16px"
+    viewBox="0 0 16 16"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+  >
+    <g id="Menu" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <rect
+        id="Rectangle"
+        fill="#D8D8D8"
+        transform="translate(8.000000, 8.000000) rotate(-45.000000) translate(-8.000000, -8.000000) "
+        x="3"
+        y="3"
+        width="10"
+        height="10"
+      ></rect>
+    </g>
+  </svg>
+</template>
---
+++ theme/vuepress-theme-cosmos/global-components/TmIconInternal.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M0.929688 8.00076L8.00076 0.929688L15.0718 8.00076L8.00076 15.0718L0.929688 8.00076ZM3 8.00076L8.00076 3L13.0015 8.00076L8.00076 13.0015L3 8.00076Z"
+    />
+  </svg>
+</template>
